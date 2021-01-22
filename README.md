74e8bc0f60ea141606d4297c9011e6fcf7aa83d0
Chris Burns <ckbzxz@gmail.com>
2021-01-22 13:51:33 -0500

    setting up db

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Project2
+
+test
--- server.js
+++ server.js
@@ -2,3 +2,17 @@
 const express = require("express");
 
 const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('app/public'));
+
+app.get("/", (req, res) => {
+    res.send("MAIN page")
+})
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+})
