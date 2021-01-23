b229d8063a7a7bf6ba53cb1f3781693d454aa58f
Chris Burns <ckbzxz@gmail.com>
2021-01-23 13:36:35 -0500

    move html/api routes to separate files

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const session = require("express-session");
 const path = require('path');
 const passport = require("./config/passport.js");
 
@@ -13,73 +14,12 @@ const db = require('./models');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
-
+app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
 
-// API Routes ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-// USERS //
-app.get("/users", (req, res) => {
-    db.Users.findAll({
-        include: [db.Posts, db.Friends]
-    }).then((result) => res.send(JSON.stringify(result)))
-})
-
-app.post("/users", (req, res) => {
-    db.Users.create({
-        first_name: req.body.first_name,
-        last_name: req.body.last_name,
-        email: req.body.email,
-        phonenumber: req.body.phonenumber,
-        password: req.body.password
-    }).then((result) => res.json(result))
-})
-
-app.delete("/users/:id", (req, res) => {
-    db.Users.destroy({
-        where: {
-            id: req.params.id
-        }
-    }).then((result) => res.json(result))
-})
-
-// POSTS //
-app.get("/posts", (req, res) => {
-    db.Posts.findAll().then((result) => res.send(JSON.stringify(result)))
-})
-
-app.post("/posts", (req, res) => {
-    db.Posts.create({
-        title: req.body.title,
-        body: req.body.body,
-        UserId: req.body.UserId
-    }).then((result) => res.json(result))
-})
-
-app.delete("/posts/:id", (req, res) => {
-    db.Posts.destroy({
-        where: {
-            id: req.params.id,
-        },
-    }).then((result) => res.json(result))
-
-})
-// Login with passport//
-app.post("/api/login", passport.authenticate("local"), (req, res) => {
-    res.json({
-        email: req.user.email,
-        id: req.user.id
-    });
-});
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-// HTML Routes ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/login.html"));
-});
+require('./routes/api-routes.js')(app);
+require('./routes/html-routes.js')(app);
 
 //add {force: true} to reset table
 db.sequelize.sync().then(() => {
