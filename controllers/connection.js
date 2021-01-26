e47c4b4e6456bd97c1c2360d9d573f4dd10b8cd6
Joyson Puvanasingham <60553801+enochj316@users.noreply.github.com>
2021-01-26 09:16:17 -0500

    connection.js

---
+++ controllers/connection.js
@@ -0,0 +1,39 @@
+/*
+-------------------------------------------------------------------------------
+Set up a connection to MySQL database and export the connection object
+-------------------------------------------------------------------------------
+*/
+
+'use strict';
+
+require('dotenv').config();
+const mysql = require('mysql');
+
+// Set up connection parameters
+const local = {
+  host: 'localhost',
+  port: process.env.PORT || 3306,
+  user: process.env.MYSQL_USER,        // in .env file
+  password: process.env.MYSQL_PASSWD,  // in .env file
+  database: 'burgers_db'
+};
+
+// const cleardb = process.env.CLEARDB_DATABASE_URL;
+// const connection = mysql.createConnection(process.env.CLEARDB_DATABASE_URL);
+
+const jawsdb = process.env.JAWSDB_URL;
+const connParams = (process.env.JAWSDB_URL) ? jawsdb : local;
+const connection = mysql.createConnection(connParams);
+
+// Attempt to connecto to the database
+connection.connect(error => {
+  if (error) {
+    console.error('ERROR: Unable to make a connection' + error.stack);
+    return;
+  }
+
+  console.log('Connected to database as ID: ' + connection.threadId);
+});
+
+// Export the connection
+module.exports = connection;
