228deb62e8a8cb84d84e2bb995e98da7f5b3d937
Kevin Sajan <kevsaj@gmail.com>
2021-01-26 00:40:51 -0500

    design

--- public/js/home.js
+++ public/js/home.js
@@ -2,7 +2,7 @@ $(document).ready(() => {
     // This file just does a GET request to figure out which user is logged in
     // and updates the HTML on the page
     $.get("/api/user_data").then(data => {
-      $("#name-top-right").text(data.email);
+      $("#name-top-right").text(data.first_name);
     });
 
     $.get("/posts").then(data => {
@@ -23,29 +23,5 @@ document.addEventListener("DOMContentLoaded", (event) => {
     profileButton.addEventListener("click", (e) => {
       e.preventDefault();
       location.replace("/user_id");
-    })
-
-    const postButton = document.getElementById("post-button");
-    const postText = document.getElementById("about");
-    postButton.addEventListener("click", (e) => {
-      e.preventDefault();
-      const postObj = {
-        title: "Title",
-        body: postText.value.trim(),
-      }
-
-      fetch("/posts", {
-        method: 'POST',
-        headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json', 
-        },
-        body: JSON.stringify(postObj)
-    }).then((response) => {
-        console.log(response)
-        location.reload();
-    }).catch(err => {
-        console.log(err)
-    })
     })
 })
