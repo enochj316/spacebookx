ffc7c8d436af7943547ab973ce1ad21266471633
Carmen Hui <karmenzkarmenz@gmail.com>
2021-01-28 21:39:53 -0500

    modified footer

--- public/js/home.js
+++ public/js/home.js
@@ -5,6 +5,28 @@ const overlay = document.querySelector('.overlay');
 const fadeElems = document.querySelectorAll('.has-fade');
 const btnWeatherSearch = document.querySelector('#weatherSearch');
 
+// Javascript for the Hamburger toggle menu
+btnHamburger.addEventListener('click', function () {
+  console.log('click hamburger');
+
+  if (header.classList.contains('open')) { // Close Hamburger Menu
+    body.classList.remove('noscroll');
+    header.classList.remove('open');
+    fadeElems.forEach(function (element) {
+      element.classList.remove('fade-in');
+      element.classList.add('fade-out');
+    });
+
+  }
+  else { // Open Hamburger Menu
+    body.classList.add('noscroll');
+    header.classList.add('open');
+    fadeElems.forEach(function (element) {
+      element.classList.remove('fade-out');
+      element.classList.add('fade-in');
+    });
+  }
+});
 
 //This function initiates the process of collecting the data from "The Weather APIs" to display on the page 
 $("#weatherSearch").on("click", function () {
@@ -52,124 +74,79 @@ $("#weatherSearch").on("click", function () {
     })
   });
 
-  // Javascript for the Hamburger toggle menu
-  btnHamburger.addEventListener('click', function () {
-    console.log('click hamburger');
-
-    if (header.classList.contains('open')) { // Close Hamburger Menu
-      body.classList.remove('noscroll');
-      header.classList.remove('open');
-      fadeElems.forEach(function (element) {
-        element.classList.remove('fade-in');
-        element.classList.add('fade-out');
-      });
-
-    }
-    else { // Open Hamburger Menu
-      body.classList.add('noscroll');
-      header.classList.add('open');
-      fadeElems.forEach(function (element) {
-        element.classList.remove('fade-out');
-        element.classList.add('fade-in');
-      });
-    }
-  });
-
   $(document).ready(() => {
-    // Javascript for the Hamburger toggle menu
-    btnHamburger.addEventListener('click', () => {
-      console.log('click hamburger');
-
-      if (header.classList.contains('open')) { // Close Hamburger Menu
-        body.classList.remove('noscroll');
-        header.classList.remove('open');
-        fadeElems.forEach((element) => {
-          element.classList.remove('fade-in');
-          element.classList.add('fade-out');
-        });
-
-      }
-      else { // Open Hamburger Menu
-        body.classList.add('noscroll');
-        header.classList.add('open');
-        fadeElems.forEach((element) => {
-          element.classList.remove('fade-out');
-          element.classList.add('fade-in');
-        });
-      }
+    // This file just does a GET request to figure out which user is logged in
+    // and updates the HTML on the page
+    $.get("/api/user_data").then(user => {
+      $("#name-top-right").text(user.first_name);
     });
 
-    $(document).ready(() => {
-      // This file just does a GET request to figure out which user is logged in
-      // and updates the HTML on the page
-      $.get("/api/user_data").then(user => {
-        $("#name-top-right").text(user.first_name);
-      });
+    const postButton = document.getElementById("post-button");
+    const postTitle = document.getElementById("post-title")
+    const postText = document.getElementById("post-body");
+    postButton.addEventListener("click", (e) => {
+      e.preventDefault();
+      const postObj = {
+        title: postTitle.value.trim(),
+        body: postText.value.trim(),
+      }
 
-      const postButton = document.getElementById("post-button");
-      const postTitle = document.getElementById("post-title")
-      const postText = document.getElementById("post-body");
-      postButton.addEventListener("click", (e) => {
-        e.preventDefault();
-        const postObj = {
-          title: postTitle.value.trim(),
-          body: postText.value.trim(),
-        }
-
-        fetch("/posts", {
-          method: 'POST',
-          headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(postObj)
-        }).then((response) => {
-          console.log(response)
-          location.reload();
-        }).catch(err => {
-          console.log(err)
-        })
-      })
-
-    });
-
-
-
-    document.addEventListener("DOMContentLoaded", (event) => {
-      console.log("DOM loaded! 🚀");
-
-      const profileButton = document.getElementById("profile-button");
-      profileButton.addEventListener("click", (e) => {
-        e.preventDefault();
-        $.get("/api/user_data").then(user => {
-          location.replace("/user/" + user.id);
-        });
-
-      })
-
-      const postButton = document.getElementById("post-button");
-      const postText = document.getElementById("about");
-      postButton.addEventListener("click", (e) => {
-        e.preventDefault();
-        const postObj = {
-          title: "Title",
-          body: postText.value.trim(),
-        }
-
-        fetch("/posts", {
-          method: 'POST',
-          headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(postObj)
-        }).then((response) => {
-          console.log(response)
-          location.reload();
-        }).catch(err => {
-          console.log(err)
-        })
+      fetch("/posts", {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(postObj)
+      }).then((response) => {
+        console.log(response)
+        location.reload();
+      }).catch(err => {
+        console.log(err)
       })
     })
-  })
+
+  });
+
+
+
+  document.addEventListener("DOMContentLoaded", (event) => {
+    console.log("DOM loaded! 🚀");
+
+    const profileButton = document.getElementById("profile-button");
+    profileButton.addEventListener("click", (e) => {
+      e.preventDefault();
+      $.get("/api/user_data").then(user => {
+        location.replace("/user/" + user.id);
+      });
+
+    })
+
+    const postButton = document.getElementById("post-button");
+    const postText = document.getElementById("about");
+    postButton.addEventListener("click", (e) => {
+      e.preventDefault();
+      const postObj = {
+        title: "Title",
+        body: postText.value.trim(),
+      }
+
+      fetch("/posts", {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(postObj)
+      }).then((response) => {
+        console.log(response)
+        location.reload();
+      }).catch(err => {
+        console.log(err)
+      })
+    })
+  }) 
+
+    
+  
 })
