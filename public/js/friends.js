d4b26fb72de3e77be1dedda139f234a1d9d38b50
Carmen Hui <karmenzkarmenz@gmail.com>
2021-01-28 23:26:45 -0500

    added hamburger menu js for friends.js

--- public/js/friends.js
+++ public/js/friends.js
@@ -1,3 +1,34 @@
+const btnHamburger = document.querySelector('#btnHamburger');
+const body = document.querySelector('body');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+const fadeElems = document.querySelectorAll('.has-fade');
+const btnWeatherSearch = document.querySelector('#weatherSearch');
+
+$(document).ready(() => {
+    // Javascript for the Hamburger toggle menu
+    btnHamburger.addEventListener('click', () => {
+      console.log('click hamburger');
+  
+      if (header.classList.contains('open')) { // Close Hamburger Menu
+        body.classList.remove('noscroll');
+        header.classList.remove('open');
+        fadeElems.forEach((element) => {
+          element.classList.remove('fade-in');
+          element.classList.add('fade-out');
+        });
+  
+      }
+      else { // Open Hamburger Menu
+        body.classList.add('noscroll');
+        header.classList.add('open');
+        fadeElems.forEach((element) => {
+          element.classList.remove('fade-out');
+          element.classList.add('fade-in');
+        });
+      }
+    });
+})
 document.addEventListener("DOMContentLoaded", (event) => {
     console.log("DOM loaded! 🚀");
 
