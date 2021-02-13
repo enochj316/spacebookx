c11b6ce1b142ea35b75090522c848cd2f79cb7a2
Kevin Sajan <kevsaj@gmail.com>
2021-02-13 17:45:49 -0500

    tailwind for production

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,23 @@
 // postcss.config.js
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  }
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+    require('@fullhuman/postcss-purgecss')({
+      content: [
+        './public/login.html',
+        './public/signup.html',
+        './views/layouts/_friends.handlebars',
+        './views/layouts/_home.handlebars',
+        './views/layouts/_user.handlebars',
+        './views/friends.handlebars',
+        './views/home.handlebars',
+        './views/user.handlebars',
+        './views/partials/friends/friends-block.handlebars',
+        './views/partials/friends/friends-list.handlebars',
+        './views/partials/moments/moment-block.handlebars',
+      ],
+      defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+    }),
+  ]
 }
